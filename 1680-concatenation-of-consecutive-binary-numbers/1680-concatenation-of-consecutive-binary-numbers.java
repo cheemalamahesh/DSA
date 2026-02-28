2c2b782ac216eec103b65b050197e906477845a6
cheemalamahesh <cheemalamahesh56@gmail.com>
2026-02-28 19:52:16 +0530

    Time: 24 ms (98.84%), Space: 42.8 MB (33.72%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.java
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.java
@@ -4,8 +4,8 @@ class Solution {
         long result=0;
         long mod=(int)1e9+7;
         for(int i =1;i<=n;i++){
-            if((i&(i-1))==0) digit++;
-            result=((result<<digit)|i)%mod;
+            if((i&(i-1))==0) digit++; //for each power of two digits increase 1-1,2-10,3-11,4-100 ..8-1000,9-1001....
+            result=((result<<digit)|i)%mod; //moving  number bits left shift base on the digit of cur number and add again the number  
         }
         return (int)result;
         
