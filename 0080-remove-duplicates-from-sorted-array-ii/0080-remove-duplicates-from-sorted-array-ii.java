cc32045d682b0c6ab88a89c457ed232585e3060c
cheemalamahesh <cheemalamahesh56@gmail.com>
2026-03-12 21:59:42 +0530

    Time: 1 ms (19.33%), Space: 48.8 MB (47.64%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,7 +1,7 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         int n=nums.length;
-        int idx=1;
+        int idx=1;//leaving the first value because if it is unige or it is twic or it is three it always start from first
         int cnt=1;
         for(int i=1;i<n;i++){
             if(nums[i]==nums[i-1]){
