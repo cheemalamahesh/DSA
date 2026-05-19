1508987ce3cf586d7fe689d751681d441170c911
cheemalamahesh <cheemalamahesh56@gmail.com>
2026-05-19 08:55:47 +0530

    Time: 14 ms (22.17%), Space: 89 MB (10.33%) - LeetHub

---
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+        HashSet<Integer> set=new HashSet<>();
+        for(int i : nums2) set.add(i);
+        for(int i : nums1){
+            if(set.contains(i)) return i;
+        }
+        return -1;
+    }
+}
