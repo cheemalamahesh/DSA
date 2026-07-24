4612c00e8cb8c57bbc855c01e3c27987ef802e64
cheemalamahesh <cheemalamahesh56@gmail.com>
2026-07-24 12:16:35 +0530

    Time: 1 ms (100%), Space: 44.3 MB (91.05%) - LeetHub

--- 3986-number-of-elapsed-seconds-between-two-times/3986-number-of-elapsed-seconds-between-two-times.java
+++ 3986-number-of-elapsed-seconds-between-two-times/3986-number-of-elapsed-seconds-between-two-times.java
@@ -1,17 +1,14 @@
 class Solution {
-    public int secondsBetweenTimes(String startTime, String endTime) {
-        String[] a1=startTime.split(":");
-        int h1=(Integer.parseInt(a1[0]))*3600;
-        int m1=(Integer.parseInt(a1[1]))*60;
-        int s1=(Integer.parseInt(a1[2]));
-        int sum1=h1+m1+s1;
-        String[] a2=endTime.split(":");
-        int h2=(Integer.parseInt(a2[0]))*3600;
-        int m2=(Integer.parseInt(a2[1]))*60;
-        int s2=(Integer.parseInt(a2[2]));
-        int sum2=h2+m2+s2;
-        return Math.abs(sum1-sum2);
-        
+    public int secondsBetweenTimes(String s1, String s2) {
+
+        int t1 = (Integer.parseInt(s1.substring(0,2))*3600 + Integer.parseInt(s1.substring(3,5)) * 60 + Integer.parseInt(s1.substring(6)));
+
+        int t2 = (Integer.parseInt(s2.substring(0,2))*3600 + Integer.parseInt(s2.substring(3,5)) * 60 + Integer.parseInt(s2.substring(6)));
+
+
+        return Math.abs(t2-t1);
+
+
         
     }
 }
